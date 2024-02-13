e9aaae26da2bd590e5c7880d52d9765bfac35639
Diana Murariu <dianamurariu@Dianas-MacBook-Pro.local>
2024-02-13 10:33:09 +0200

    Add math operand functions

---
+++ script.js
@@ -0,0 +1,8 @@
+// Math operator functions
+const operations = {
+  add: (num1, num2) => num1 + num2,
+  subtract: (num1, num2) => num1 - num2,
+  multiply: (num1, num2) => num1 * num2,
+  divide: (num1, num2) =>
+    num2 !== 0 ? num1 / num2 : 'Error: Cannot divide by zero!',
+};
